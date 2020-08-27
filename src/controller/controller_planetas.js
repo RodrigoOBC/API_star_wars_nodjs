58c66d4e0407d57d7e033fb01b2461c4f989046d
RodrigoOBC <rodrigooliveiracabral@yahoo.com.br>
2020-08-27 19:59:10 -0300

    Primeira interação

--- src/controller/controller_planetas.js
+++ src/controller/controller_planetas.js
@@ -5,5 +5,9 @@ const router = express.Router()
 router.post('/inserir_planeta', async (res,req) => {
     try {
         const planeta = Planetas.create(req.body)
+
+        return res.status(201).send({ planeta })
+    }catch(err){
+        return res.status(404).send({ erro: "Erro ao cadastrar Planeta" })
     }
 })
